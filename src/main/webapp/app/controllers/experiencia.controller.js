4912fa45a61aadd9182305e6cfeb9340941f05a7
clopez@apci.gob.pe <clopez@apci.gob.pe>
2015-09-30 18:08:44 -0500

    Version alpha 4

--- src/main/webapp/app/controllers/experiencia.controller.js
+++ src/main/webapp/app/controllers/experiencia.controller.js
@@ -16,7 +16,7 @@ angular.module("main").controller("ExperienciaController" , function(Utils, APP)
 
 	this.guardar = function(){
 
-<<<<<<< HEAD
+
 		Utils.Validation.init();
 		Utils.Validation.required("#date-fechainicio","Fecha inicio");
 		Utils.Validation.required("#date-fechafin","Fecha fin");
@@ -30,80 +30,53 @@ angular.module("main").controller("ExperienciaController" , function(Utils, APP)
 		if (Utils.Validation.run()) {
 		
 		
-		this.experiencia.fechaInicio = moment(this.experiencia.fechaInicio,"DD/MM/YYYY").toDate();
-		this.experiencia.fechaFin = moment(this.experiencia.fechaFin,"DD/MM/YYYY").toDate();
-
-		console.log("fecha inicio ",this.experiencia.fechaInicio);
-		console.log("fecha fin ",this.experiencia.fechaFin);
+			this.experiencia.fechaInicio = moment(this.experiencia.fechaInicio,"DD/MM/YYYY").toDate();
+			this.experiencia.fechaFin = moment(this.experiencia.fechaFin,"DD/MM/YYYY").toDate();
+			this.experiencia.registrada = true;
+			
+			Utils.Rest.update(APP.URL_API + "experiencia", this.experiencia);
+					
+			$('input[type="file"]').each(function(index, control)
+				{
+					if ($(control).attr("ok") === "true") 
+					{
+						control.disabled="disabled"
+						var archivoExperiencia = {};
+						archivoExperiencia.inscripcionExperienciaId = self.experiencia.inscripcionExperienciaId;
+						archivoExperiencia.archivo = control.files[0].name;
+						archivoExperiencia.tipoArchivo = S(control.files[0].name).left(1).toUpperCase().s;
+						Utils.Rest.save(APP.URL_API + "archivoexperiencia", archivoExperiencia);
+					}
+				});
+			}
 		
-=======
->>>>>>> 6795b0cddfdf3f7efba4b4150783372f5b5d0d8a
-		this.experiencia.registrada = true;
-		Utils.Rest.update(APP.URL_API + "experiencia", this.experiencia);
-		
-		
-		this.experiencia.fechaInicio = null;
-		this.experiencia.fechaFin = null;
-		this.experiencia.sumilla = null;		
-		$(".modal-registrar-experiencia").modal("hide");		
+			this.experiencia.fechaInicio = null;
+			this.experiencia.fechaFin = null;
+			this.experiencia.sumilla = null;		
+			$(".modal-registrar-experiencia").modal("hide");
+			
 		}
 		
-		
+		$('input[type="file"]').change(function(){
 			
-	$('input[type="file"]').each(function(index, control)
-		{
-			if ($(control).attr("ok") === "true") 
+			console.log("upload",this);
+			var control = this;
+			$("#" + control.id + "-ok" ).hide();
+			$(control).attr("ok","false");
+			var	loaded = Utils.File.upload(control, APP.URL_API + "file/upload/" + self.experiencia.inscripcionExperienciaId );
+			if (loaded) 
 			{
-				
-				control.disabled="disabled"
-				var archivoExperiencia = {};
-				archivoExperiencia.inscripcionExperienciaId = self.experiencia.inscripcionExperienciaId;
-				archivoExperiencia.archivo = control.files[0].name;
-				archivoExperiencia.tipoArchivo = S(control.files[0].name).left(1).toUpperCase().s;
-				Utils.Rest.save(APP.URL_API + "archivoexperiencia", archivoExperiencia);
+				loaded.success(function(){
+					$(control).attr("ok","true");
+					$("#" + control.id + "-ok" ).show();
+				});
 			}
 		});
-		
-<<<<<<< HEAD
-		//Utils.UI.Control.disableChilds("form-experiencia");
-	}
 	
-		
-=======
-		Utils.UI.Control.disableChilds("form-experiencia");
-		this.experiencia = {};
-		$(".modal-registrar-experiencia").modal("hide");
-	}
-	
->>>>>>> 6795b0cddfdf3f7efba4b4150783372f5b5d0d8a
-	$('input[type="file"]').change(function(){
-		
-		console.log("upload",this);
-		var control = this;
-		$("#" + control.id + "-ok" ).hide();
-		$(control).attr("ok","false");
-		var	loaded = Utils.File.upload(control, APP.URL_API + "file/upload/" + self.experiencia.inscripcionExperienciaId );
-		if (loaded) 
-		{
-			loaded.success(function(){
-				$(control).attr("ok","true");
-				$("#" + control.id + "-ok" ).show();
-			});
-		}
-	});
-<<<<<<< HEAD
-	
-			
-	
-
-=======
-	
-	$(".modal-registrar-experiencia").on('hidden.bs.modal', function (e) {
-		
-		self.experiencia = {};
-		$('input[type="file"]').value('');
-		
-	})
->>>>>>> 6795b0cddfdf3f7efba4b4150783372f5b5d0d8a
-	
+//		$(".modal-registrar-experiencia").on('hidden.bs.modal', function (e) {
+//			
+//			self.experiencia = {};
+//			$('input[type="file"]').value('');
+//			
+//		});
 });
